ab1666a5d37fbb3594dd28c4800a555bc2d680f3
mantosh293 <mantosh293@gmail.com>
2026-06-15 23:19:12 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const { currentUser } = require('./middleware/auth');
 const { sanitizeBody } = require('./utils/helpers');
 
 const app = express();
-const store = new MySQLStore({}, require('./config/db').pool);
+//const store = new MySQLStore({}, require('./config/db').pool);
 
 app.set('trust proxy', 1);
 app.use(helmet({ contentSecurityPolicy: false }));
@@ -21,7 +21,7 @@ app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 500 }));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(session({ name: 'ankita.sid', secret: config.sessionSecret, resave: false, saveUninitialized: false, store, cookie: { httpOnly: true, sameSite: 'lax', secure: config.env === 'production', maxAge: 1000 * 60 * 60 * 8 } }));
+app.use(session({ name: 'ankita.sid', secret: config.sessionSecret, resave: false, saveUninitialized: false, cookie: { httpOnly: true, sameSite: 'lax', secure: config.env === 'production', maxAge: 1000 * 60 * 60 * 8 } }));
 app.use(currentUser);
 app.use('/api', sanitizeBody);
 app.use(express.static(path.join(config.root, 'public')));
